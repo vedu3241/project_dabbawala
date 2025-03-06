c9ad543726bb2f43c3f8bb47139c3de57cae7b4b
vedu3241 <parulekarvedant12345@gmail.com>
2025-03-06 18:28:11 +0530

    added post feed in home

--- lib/features/HomePage/screens/home_page.dart
+++ lib/features/HomePage/screens/home_page.dart
@@ -1,52 +1,68 @@
+import 'package:dabbawala/features/HomePage/widgets/user_post.dart';
 import 'package:dabbawala/features/NotificationPage/screens/notificato_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
-
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  List<String> users = [
+    'Vedant',
+    'Zubair',
+    'Mit',
+    'Saim',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          actions: [
-            IconButton(
-              icon: const Icon(
-                  color: Colors.white, Icons.notifications, size: 28.0),
-              onPressed: () {
-                Get.to(NotificatonPage());
-              },
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+            icon: const Icon(
+                color: Colors.white, Icons.notifications, size: 28.0),
+            onPressed: () {
+              Get.to(NotificatonPage());
+            },
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.qr_code_sharp,
+              color: Colors.white,
             ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.qr_code_sharp,
-                color: Colors.white,
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'dabbawalas'.tr,
+              style: GoogleFonts.acme(color: Colors.black, fontSize: 20),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  return UserPost(
+                    name: users[index],
+                  );
+                },
               ),
             )
           ],
         ),
-        body: SingleChildScrollView(
-            child: SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Container(
-                  child: Text(
-                  
-                        'dabbawalas'.tr,
-                    style: GoogleFonts.acme(color: Colors.black, fontSize: 20),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        );
-        
+      ),
+    );
   }
 }
---
+++ lib/features/HomePage/widgets/user_post.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class UserPost extends StatelessWidget {
+  const UserPost({super.key, required this.name});
+
+  final String name;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              // User PFP and name
+              Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.grey[300], shape: BoxShape.circle),
+              ),
+              const SizedBox(width: 10),
+              Text(
+                name,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const Spacer(),
+              //more option
+              Icon(Icons.more_horiz)
+            ],
+          ),
+        ),
+        // Post image
+        Container(
+          height: 400,
+          color: Colors.grey[300],
+        ),
+        // Below post buttons
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 5.0),
+                child: Icon(Icons.favorite_border),
+              ),
+              Text("223"),
+              const SizedBox(width: 12),
+              Padding(
+                padding: const EdgeInsets.only(right: 5.0),
+                child: Icon(Icons.chat_bubble_outline),
+              ),
+              Text("7"),
+              const SizedBox(width: 12),
+              Icon(Icons.send)
+            ],
+          ),
+        ),
+
+        //Caption
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: RichText(
+              text: TextSpan(
+            children: [
+              TextSpan(
+                  text: "${name} ",
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold)),
+              TextSpan(
+                  text:
+                      "Delivering happiness, one meal at a time! 🚲✨ #DabbawalaDiaries #MumbaiMagic #TiffinTales",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ))
+            ],
+          )),
+        )
+      ],
+    );
+  }
+}
